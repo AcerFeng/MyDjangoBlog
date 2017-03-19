7df21a8fe9d0188cbac784292f3daecfd2c119ff
AcerFeng <qinlanfeng@foxmail.com>
2017-03-20 02:01:30 +0800

    index

---
+++ myblog/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from . import models
+
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', {'hello':'hello bloghahaha.'})
+
+def test(request):
+    return render(request, 'blog/test.html')
